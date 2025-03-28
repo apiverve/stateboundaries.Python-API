c2fafeece0ea63c32c90de9028fa71421c3bb315
APIVerve Team <hello@apiverve.com>
2025-03-28 17:52:20 -0500

    Bug Fixes

---
+++ src/apiverve_stateboundaries/setup_dep.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='apiverve_stateboundaries',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'setuptools'
+    ],
+    description='State Boundaries is a simple tool for getting the boundaries of a specific state's border. It returns the shape of the specified state's border.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
